51e76218de1837ce5502dfacec0824f7176b05c3
J-IOProgrammer <fg.bonny@outlook.com>
2022-08-27 10:32:08 +0430

    added factorial method

--- src/fatemeh.java
+++ src/fatemeh.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class fatemeh {
+    public static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println("fatemeh");
+        System.out.println("enter a number to calculate it's factorial : ");
+        System.out.println("the fact is : " + Factorial(scanner.nextInt()));
+    }
+
+    public static int Factorial(int number) {
+        if (number == 1)
+            return 1;
+        return number * Factorial(number - 1);
     }
 }
